98a7babc7bfa492e6f450544f233512c7b223bfc
myagmarbattur-cloud <myagmarbattur@gmail.com>
2026-08-26 07:27:14 +0800

    Update channels.js

--- api/channels.js
+++ api/channels.js
@@ -1,1 +1,16 @@
+export default async function handler(req, res) {
+  const guildId = "1529096653372522596";
 
+  const response = await fetch(
+    `https://discord.com/api/v10/guilds/${guildId}/channels`,
+    {
+      headers: {
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`
+      }
+    }
+  );
+
+  const channels = await response.json();
+
+  res.status(response.status).json(channels);
+}
